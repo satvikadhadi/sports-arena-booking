8724076d321ca84654cb5440f42a5ef338ae8713
satvikadhadi <pranavidhadi@gmail.com>
2026-05-05 20:47:52 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/lib/authContext";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookArena from "./pages/BookArena";
+import UserDashboard from "./pages/UserDashboard";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AuthProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/book/:id" element={<BookArena />} />
+            <Route path="/dashboard" element={<UserDashboard />} />
+            <Route path="/owner" element={<OwnerDashboard />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
